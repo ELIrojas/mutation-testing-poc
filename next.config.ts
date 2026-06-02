43b9dee0ea1370208551caebbc92813e631e2f68
ELIrojas <rojasellie14@gmail.com>
2026-06-02 11:58:43 -0500

    feat: add github pages deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath:"/mutation-testing-poc",
+  images:{unoptimized:true},
 };
 
 export default nextConfig;
