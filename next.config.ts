185c1d5be6477404dcc60e15ae9b2e83de0b52a1
ELIrojas <rojasellie14@gmail.com>
2026-06-02 17:03:58 -0500

    ui updates

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   basePath:"/mutation-testing-poc",
+//   images:{unoptimized:true},
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath:"/mutation-testing-poc",
-  images:{unoptimized:true},
+  basePath: process.env.NODE_ENV === "production" ? "/mutation-testing-poc" : "",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
